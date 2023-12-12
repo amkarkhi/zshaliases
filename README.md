393e3345679983bf1cdfa680570daf1c916a0043
amkarkhi <amkarkhi@gmail.com>
2023-12-12 18:52:04 +0330

    Edit and stuff

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Info
+
 Use this repo to clone in `oh-my-zsh` source it how ever you like
 
 ```sh
 git clone git@github.com:amkarkhi/zshaliases.git ~/.oh-my-zsh/custom/amin
 
 # add command to your custom files
-echo 'source ~/.oh-my-zsh/custom/amin/aliases.zsh' >> ~/.oh-my-zsh/custom/example.zsh
+echo 'source ~/$pwd/aliases.zsh' >> ~/.zshrc
 ```
 
 This is not gonna be anything fancy but here it is!
+
+# task to do
+
+[ ] check if we are in a session and then switch between sessions.
+
+[ ] add ssh session to it if needed
--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,21 @@
+# add this to 'source $pwd/.aliases' to '~/.zshrc'
 # get a param to check if there is an available session or not and create a new one if needed
 function tmas
 {
   if [ -z $1 ]; then
-    local res=$(tmux list-session |fzf --reverse --prompt='tmux attach session: '|cut -d: -f1 | xargs echo)
-    if [ -z $res ]; then return; else tmux attach-session -t $res; fi
+    local res=$(tmux ls |fzf --reverse --prompt='tmux attach session: '|cut -d: -f1 | xargs echo)
+    if [ -z $res ]; then echo "No session"; return; else tmux attach-session -t $res; fi
   else
-    local fff=$(tmux list-session| grep $1)
-    if [ -z $fff ]; then
+    local res=$(tmux list-session| grep $1)
+    if [ -z $res ]; then
       tmux new -s $1;
     else
       local res=$(tmux list-session|grep $1 |fzf --reverse --prompt='tmux attach session: '|cut -d: -f1 | xargs echo)
       if [ -z $res ]; then return; else tmux attach-session -t $res; fi
     fi
   fi
-
+  return ;
 }
+
+alias open=xdg-open
+
